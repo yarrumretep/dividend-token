6ea18c49dbfc627afad5ee52c8adaaf1fdce1bed
Peter Murray <pete@pmurray.com>
2018-02-07 19:59:30 -0500

    adding tests.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const expect = require('expect');
+
+const DividendToken = artifacts.require('./DividendToken.sol');
+
+contract("DividendToken", () => {
+  it('should have a test', () => {
+    expect(true).toBe(false, "Need to write tests!");
+  })
+})
